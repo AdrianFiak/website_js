2d7dbefe2883882935610e4b2c30eb3c3463402b
AdrianFiak <adrian.fiak@yahoo.ro>
2022-03-30 12:09:31 +0300

    teste

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ let nrMeu=500;
 console.log(nrMeu, typeof nrMeu);
 let nrNou='500';
 console.log(nrNou, typeof nrNou);
+
+
+const numar=1;//constantei i se atribuie o valoare la declarare si schimbarea acesteia rezulta intr-o eroare
+console.log('numar:',numar);
+
+const animal={ rasa:'caine'};//valoarea constantei este un obiect
+console.log(animal.rasa);
+animal.rasa='magar';// unei constante i se poate totusi modifica valoarea cand aceasta este un obiect si i se modifica de fapt valoarea obiectului
+console.log(animal.rasa);
+
+const myInt = 5;
+const myFloat = 6.667;
+const douaZecimale = myFloat.toFixed(2);
+console.log(douaZecimale);
